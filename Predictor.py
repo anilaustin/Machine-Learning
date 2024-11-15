88e2d3e4bca265d06da6576ff198c63dec31ba83
anilaustin <anilaustin17@gmail.com>
2024-11-15 13:05:51 +0530

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -1,20 +1,25 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-
 import pandas as pd
 import numpy as np
 import requests
 import pickle
+from io import BytesIO
 
-model_url = 'https://raw.githubusercontent.com/Srikanth1316/Machine-Learning/master/model_pickle2'
-response = requests.get(model_url, stram = True)
+
+model_url = 'https//github.com/anilaustin/Machine-Learning/master/model_pickle2'
+response = requests.get(model_url, stream=True)
 response.raise_for_status()
 
-    model = pickle.load(BytesIO(response.content))
+
+model = pickle.load(BytesIO(response.content))
 
 
-def predict_house_price(area,bedrooms,age):
-    input_data =[[area,bedrooms,age]]
+def predict_house_price(area, bedrooms, age):
+    input_data = [[area, bedrooms, age]]
     predicted_price = model.predict(input_data)
     return predicted_price[0]
+
+
+
